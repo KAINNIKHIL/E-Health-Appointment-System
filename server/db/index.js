878a9773169fb402777658b5ee9b57d13dceee1e
Nikhil <nikhilsinghkaintura@gmail.com>
2025-10-07 22:53:51 +0530

    fix: ensure default MySQL port is set correctly in Sequelize configuration

--- server/db/index.js
+++ server/db/index.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASS,
   {
     host: process.env.MYSQL_HOST || "localhost",
+     port: process.env.MYSQL_PORT || 3306,
     dialect: "mysql",
     logging: false,
   }
